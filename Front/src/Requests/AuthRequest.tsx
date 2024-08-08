3d0964c8fe06f8e40b8d9389a2731b3e63579d5c
NightDev83 <dylan.bouraoui83@gmail.com>
2024-08-08 11:09:38 +0200

    push

--- Front/src/Requests/AuthRequest.tsx
+++ Front/src/Requests/AuthRequest.tsx
@@ -1,36 +1,48 @@
-import axios from 'axios';
-import { LoginRequestBody, LoginResponse, RegisterRequestBody, RegisterResponse } from '../Types/auth';
-
+import axios from "axios";
+import {
+  LoginRequestBody,
+  LoginResponse,
+  RegisterRequestBody,
+  RegisterResponse,
+} from "../Types/auth";
 
 export const register = async (
-    firstname: string,
-    lastname: string,
-    email: string,
-    password: string
+  firstname: string,
+  lastname: string,
+  email: string,
+  password: string
 ): Promise<RegisterResponse> => {
-    const requestBody: RegisterRequestBody = { firstname, lastname, email, password };
+  const requestBody: RegisterRequestBody = {
+    firstname,
+    lastname,
+    email,
+    password,
+  };
 
-    try {
-        const response = await axios.post<RegisterResponse>(`/api/register`, requestBody);
-        return response.data;
-    } catch (error: any) {
-        return error.response.data.message || "echec de l'inscription";
-        
-    }
+  try {
+    const response = await axios.post<RegisterResponse>(
+      `http://127.0.0.1:8000/api/register`,
+      requestBody
+    );
+    return response.data;
+  } catch (error: any) {
+    return error.response.data.message || "echec de l'inscription";
+  }
 };
 
-
-
 export const login = async (
-    email: string,
-    password: string
+  email: string,
+  password: string
 ): Promise<LoginResponse> => {
-    const requestBody: LoginRequestBody = { email, password };
+  const requestBody: LoginRequestBody = { email, password };
 
-    try {
-        const response = await axios.post<LoginResponse>(`/api/login_check`, requestBody);
-        return response.data;
-    } catch (error: any) {
-        return error.response?.data?.message || 'Échec de la connexion';
-    }
+  try {
+    const response = await axios.post<LoginResponse>(
+      `http://127.0.0.1:8000/api/login_check`,
+      requestBody
+    );
+    return response.data;
+  } catch (error: any) {
+    return error.response?.data?.message || "Échec de la connexion";
+  }
 };
